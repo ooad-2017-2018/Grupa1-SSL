99ece79d19b12ddf295ab6cfed7dc7e27e36195a
harisajdinovic <hajdinovic1@etf.unsa.ba>
2018-05-20 15:05:37 +0200

    dodana baza

--- Projekat/SarajevoSocialLife/SarajevoSocialLife/App.xaml.cs
+++ Projekat/SarajevoSocialLife/SarajevoSocialLife/App.xaml.cs
@@ -14,6 +14,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Microsoft.WindowsAzure.MobileServices;
+
 
 namespace SarajevoSocialLife
 {
@@ -22,10 +24,15 @@ namespace SarajevoSocialLife
     /// </summary>
     sealed partial class App : Application
     {
+
+
+        public static MobileServiceClient MobileService = new
+        MobileServiceClient("https://SarajevoSocialLife.azurewebsites.net");
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
         /// </summary>
+        /// 
         public App()
         {
             this.InitializeComponent();
