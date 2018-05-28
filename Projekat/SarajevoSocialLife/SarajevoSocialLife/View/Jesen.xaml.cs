08bf00204e04467c1f28f7870bd68363ace8befa
samrabejtic <sbejtic1.etf.unsa.ba>
2018-05-28 14:42:40 +0200

    ok

--- Projekat/SarajevoSocialLife/SarajevoSocialLife/View/Jesen.xaml.cs
+++ Projekat/SarajevoSocialLife/SarajevoSocialLife/View/Jesen.xaml.cs
@@ -20,25 +20,24 @@ namespace SarajevoSocialLife.View
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class Proljece : Page
+    public sealed partial class Jesen : Page
     {
-        public Proljece()
+        public Jesen()
         {
             this.InitializeComponent();
         }
 
-      
-
-     
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
-           // aplikacija = (Aplikacija)e.Parameter;
+            // aplikacija = (Aplikacija)e.Parameter;
         }
 
         private void nazad_Click(object sender, RoutedEventArgs e)
         {
             this.Frame.Navigate(typeof(PocetnaForma));
         }
+
+       
     }
 }
